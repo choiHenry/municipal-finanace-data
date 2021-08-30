462a8c127dfd142dff25529e5e830e0a70f44603
Henry <51810088+choiHenry@users.noreply.github.com>
2021-08-30 13:13:37 +0900

    Update auxilary.py

--- auxilary.py
+++ auxilary.py
@@ -1,51 +1,7 @@
-from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 import os
 import requests
 import pandas as pd
-
-
-def check_exists_by_link_text(webdriver, link_text):
-    try:
-        webdriver.find_element_by_link_text(link_text)
-    except NoSuchElementException:
-        return False
-    return True
-
-
-def check_exists_by_xpath(webdriver, xpath):
-    try:
-        webdriver.find_element_by_xpath(xpath)
-    except NoSuchElementException:
-        return False
-    return True
-
-def download_wait(path_to_downloads):
-    seconds = 0
-    dl_wait = True
-    while dl_wait and seconds < 20:
-        sleep(1)
-        for fname in os.listdir(path_to_downloads):
-            if fname.endswith('.xls'):
-                dl_wait = False
-        seconds += 1
-    if dl_wait:
-        print("Download failed.")
-        return
-    print(f"Download was successful in {seconds} seconds.")
-
-def page_load_wait(wd, xpath, threshold):
-    seconds = 0
-    pl_wait = True
-    while pl_wait and seconds < 20:
-        sleep(1)
-        if len(wd.find_elements_by_xpath(xpath)) > threshold:
-            pl_wait = False
-        seconds += 1
-    if pl_wait:
-        print("Faild to load a page.")
-        return
-    print(f"Page loading was successful in {seconds} seconds: {xpath}")
 
 def erase_all_with_extension(path, ext):
     files_in_directory = os.listdir(path)
